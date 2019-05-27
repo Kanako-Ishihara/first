fd7ef26f8bf68e2a50ffadf3f7f70bf3115a4439
ishihara <ishihara@key-p.jp>
2019-05-27 10:15:23 +0900

    初期状態

---
+++ insert_Judge.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>insert</title>
+</head>
+
+<body>
+  <button type="button" onclick="location.href='./list.php'">一覧に戻る</button>
+</body>
+</html>
