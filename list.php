713e68a9e1bf0cef5702b6e8f8cf13def3ccf48d
ishihara <ishihara@key-p.jp>
2019-05-27 14:24:34 +0900

    SQLでリスト化成功

--- list.php
+++ list.php
@@ -10,18 +10,21 @@
     require_once('./helpers/db_helper.php');
 
     $dbh = get_db_connect();
-    $sql = 'SELECT code as 社員番号,name as 社員名, name_kana as "社員名 かな",gender FROM user';
+    $sql = 'SELECT code as 社員番号,name as 社員名, name_kana as "社員名 かな",
+      case gender when 1 then "男" when 2 then "女" else "不明" end as "性別",
+      created_at as 登録日, updated_at as 更新日 FROM user';
+    echo $sql.'<br />';
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         $data[] = $row;
     }
-    var_dump($data);
+    var_dump($data[0]);
  ?>
 
 <body>
-  <button type="button" onclick="location.href=''">追加</button> //未実装
-
+  <button type="button" onclick="location.href=''">追加</button>
+  <!-- 未実装 -->
   <br>
 
   <div>社員一覧表</div>
@@ -36,18 +39,16 @@
       <td>編集</td>
       <td>削除</td>
     </tr>
-
-    <tr>
-      <td>1</td>
-      <td>平川勇太</td>
-      <td>ひらかわゆうた</td>
-      <td>男性</td>
-      <td>2018-12-1 09:00:00</td>
-      <td>2018-12-1 09:00:00</td>
-      <td><button type="button">編集</button></td>
-      <td><button type="button">削除</button></td>
-    </tr>
-
+<?php echo count($data);
+foreach ($data as $emp_num => $emp_data) { ?>
+  <tr>
+    <?php foreach ($emp_data as $key => $value){?>
+      <td><?php echo $value; ?></td>
+    <?php } ?>
+    <td><button type="button">編集</button></td>
+    <td><button type="button">削除</button></td>
+  </tr>
+<?php } ?>
   </table>
 </body>
 </html>
